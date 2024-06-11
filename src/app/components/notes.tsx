cc8301569c997869eb5ff96dd259f04209341a0f
Louis Ricardo Supit <t-louis.supit@traveloka.com>
2024-06-11 14:14:49 +0700

    feat: add supabase client hook

--- src/app/components/notes.tsx
+++ src/app/components/notes.tsx
@@ -1,18 +1,17 @@
 // 1
 "use client";
 
-import { createClient } from '@/src/app/utils/supabase/server';
+import { createClient } from "@/src/app/utils/supabase/client";
 // 2
-import { useParams } from 'next/navigation';
-
+import { useParams } from "next/navigation";
 
 export default async function Notes() {
-    // 3
-    const params = useParams<{ brands: string }>();
-    console.log(params);
+  // 3
+  const params = useParams<{ brands: string }>();
+  console.log(params);
 
-    const supabase = createClient();
-    const { data: notes } = await supabase.from("notes").select();
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
 
-    return <pre>{JSON.stringify(notes, null, 4)}</pre>
+  return <pre>{JSON.stringify(notes, null, 4)}</pre>;
 }
